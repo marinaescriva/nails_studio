bad485042918490a3e9710be5c38ae3c5d208e7b
marinaescriva <marinaescriva.24@gamil.com>
2024-02-27 09:56:18 +0100

    feature: adding first migration for users

---
+++ src/database/migrations/1709022178841-authentification.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Authentification1709022178841 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 
 import express, { Application } from "express";
 import dotenv from "dotenv";
-import {getUsers, getUsersProfile, putUsersProfile} from "./controllers/usersController"; 
+import { getUsers, getUsersProfile, putUsersProfile } from "./controllers/usersController";
 import { getServices } from "./controllers/servicesController";
 import { postAuthLogin, postAuthRegister } from "./controllers/authentificationController";
 import { getAppointments, getAppointmentsbyID, postAppointments, putAppointments } from "./controllers/appointmentsController";
+import { AppDataSource } from "./database/db";
 
 dotenv.config();
 
@@ -13,20 +14,31 @@ const app: Application = express();
 const PORT = process.env.PORT || 4001;
 
 
-app.get(`/users`, getUsers );
+app.get(`/users`, getUsers);
 app.get(`/users/profile`, getUsersProfile);
 app.put(`/users/profile`, putUsersProfile);
 
 app.post(`/auth/login`, postAuthLogin);
-app.post(`/auth/register`, postAuthRegister); 
+app.post(`/auth/register`, postAuthRegister);
 
 app.get(`/appointments/{id}`, getAppointments);
-app.get(`/appointments`, getAppointmentsbyID );
-app.post(`/appointments`, postAppointments );
+app.get(`/appointments`, getAppointmentsbyID);
+app.post(`/appointments`, postAppointments);
 app.put(`/appointments`, putAppointments);
 
 app.get(`/services`, getServices);
 
-app.listen(PORT,() =>{
-    console.log(`server is running on port: ${PORT}`);
-});
+AppDataSource.initialize()
+    .then(() => {
+        console.log(`Database connected`);
+
+        app.listen(PORT, () => {
+            console.log(`server is running on port: ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+
+
