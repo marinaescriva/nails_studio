71be197823a7b7e29f80c4138c6022271f1e288c
Marina <marinaescriva.24@gmail.com>
2024-02-27 09:56:18 +0100

    feature: adding first migration for users

---
+++ src/database/db.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { Users1709021870692 } from "./migrations/1709021870692-users";
+// import { Appointments1709022146028 } from "./migrations/1709022146028-appointments";
+// import { Authentification1709022178841 } from "./migrations/1709022178841-authentification";
+// import { Services1709022161726 } from "./migrations/1709022161726-services";
+
+import "dotenv/config"
+
+export const AppDataSource = new DataSource({
+type: "mysql",
+host: process.env.DB_HOST || "localhost",
+port: Number(process.env.DB_PORT)||3308,
+username: process.env.DB_USER || "root",
+password: process.env.DB_USER_PASSWORD ||"",
+database: process.env.DB_DATABASE ||"test",
+entities: [],
+migrations:[Users1709021870692],
+synchronize: false,
+logging: false,
+});
+
+// Appointments1709022146028,Authentification1709022178841,Services1709022161726  ],
