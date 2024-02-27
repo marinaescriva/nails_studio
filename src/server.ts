8047c9312196167a87da8cfc8e5895d950c7908f
marinaescriva <marinaescriva.24@gamil.com>
2024-02-27 16:47:05 +0100

    update: app.ts added to separate files from server

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,17 @@
 
-import express, { Application } from "express";
-import dotenv from "dotenv";
-import { getUsers, getUsersProfile, putUsersProfile } from "./controllers/usersController";
-import { getServices } from "./controllers/servicesController";
-import { postAuthLogin, postAuthRegister } from "./controllers/authentificationController";
-import { getAppointments, getAppointmentsbyID, postAppointments, putAppointments } from "./controllers/appointmentsController";
+import 'dotenv/config'
+import { app } from './app';
+
 import { AppDataSource } from "./database/db";
-
-dotenv.config();
-
-const app: Application = express();
 
 const PORT = process.env.PORT || 4001;
 
 
-app.get(`/users`, getUsers);
-app.get(`/users/profile`, getUsersProfile);
-app.put(`/users/profile`, putUsersProfile);
-
-app.post(`/auth/login`, postAuthLogin);
-app.post(`/auth/register`, postAuthRegister);
-
-app.get(`/appointments/{id}`, getAppointments);
-app.get(`/appointments`, getAppointmentsbyID);
-app.post(`/appointments`, postAppointments);
-app.put(`/appointments`, putAppointments);
-
-app.get(`/services`, getServices);
-
-AppDataSource.initialize()
+const startServer = () => {
+    
+    AppDataSource.initialize()
     .then(() => {
+
         console.log(`Database connected`);
 
         app.listen(PORT, () => {
@@ -37,8 +19,11 @@ AppDataSource.initialize()
         });
     })
     .catch(error => {
-        console.log(error);
-    });
 
+        console.log(error);
+    })
+};
+
+startServer();
 
 
