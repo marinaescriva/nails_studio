bca2715ec1583ad5757e051e723b21e4b1355e7f
marinaescriva <marinaescriva.24@gamil.com>
2024-02-28 20:08:27 +0100

    modify the appointments migration and error in appointment_date solved

---
+++ src/models/Appointment.ts
@@ -0,0 +1,28 @@
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./User";
+import { Service } from "./Service";
+
+@Entity()
+export class Appointment extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column({ name: 'appointment_date' })
+    appointmentDate!: Date
+
+    @Column({ name: 'user_id' })
+    userId!: string
+
+    @Column({ name: 'service_id' })
+    serviceId!: string
+
+    
+    @ManyToOne(() => User, (user) => user.appointments)
+    @JoinColumn({ name: 'user_id' })
+    user!: User;
+
+    @ManyToOne(() => Service, (service) => service.appointments)
+    @JoinColumn({ name: 'service_id' })
+    service!: Service;
+}
