cf935d41042fe2de41d5b47a280b84f9d5f5a843
Marina <marinaescriva.24@gmail.com>
2024-02-28 21:06:33 +0100

    editing user controller defining allowsenses

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,31 +1,58 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { User } from "../models/User";
+import { Role } from "../models/Role";
+// import bcrypt from "bcrypt";
 
-export const getUsers = (req:Request, res:Response) => {
+export const getUsers = async(req: Request, res: Response) => {
 
-    res.status(200).json(  //solo debe ser para super_admin
-        {
-            success:true,
-            message: "ver todos los usuarios"
-        }
-    )
+    try {
+        const users = await User.find({
+
+            select: {
+                id: true,
+                name: true,
+                surname: true,
+                email: true
+            }
+
+        });
+        if(req.users.roleID == 1) 
+        res.status(200).json(  //solo debe ser para super_admin
+            {
+                success: true,
+                message: "users retrieved successfully",
+                data: users
+            }
+        )
+    }catch(error){
+        res.status(200).json(
+            {
+                success: false,
+                message: "users can't be retrieved",
+                error: error
+            }
+        )
+
+    }
 };
-export const getUsersProfile = (req:Request, res:Response)=>{
 
-    res.status(200).json(
-        {
-            success:true,
-            message: "ver perfil de usuario"
-        }
-    )
+    export const getUsersProfile = (req: Request, res: Response) => {
 
-};
-export const putUsersProfile = (req:Request, res:Response)=>{
+        res.status(200).json(
+            {
+                success: true,
+                message: "v"
+            }
+        )
 
-    res.status(200).json(
-        {
-            success:true,
-            message: "modificar un campo del perfil"
-        }
-    )
+    };
+    export const putUsersProfile = (req: Request, res: Response) => {
 
-}
+        res.status(200).json(
+            {
+                success: true,
+                message: "modificar un campo del perfil"
+            }
+        )
+
+    };
